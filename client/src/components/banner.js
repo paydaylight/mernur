d900b0fdf0de8987ad7be1521fa5fda457611647
paydaylight <maxat235@gmail.com>
2020-02-07 10:41:03 +0600

    added ws in client

--- client/src/components/banner.js
+++ client/src/components/banner.js
@@ -2,20 +2,39 @@ import React from 'react';
 import Cell from './cell'
 
 class Banner extends React.Component {
-    ws = new WebSocket('ws://localhost:3000/ws')
+    constructor(props) {
+        super(props)
+        this.state = {
+            currencies: [
+                { name: 'USD', buy: 0, sell: 0 }, 
+                { name: 'EUR', buy: 0, sell: 0 },
+                { name: 'RUB', buy: 0, sell: 0 },
+                { name: 'KGS', buy: 0, sell: 0 },
+                { name: 'GBP', buy: -1, sell: 0 },
+                { name: 'CNY', buy: 0, sell: 0 }
+            ], 
+            updated_at: Date.now().toString()
+        }
+    }
+    ws = new WebSocket('ws://localhost:2222')
 
     componentDidMount() {
         this.ws.onopen = () => {
-            try{
-                this.ws.send({message: "start"})
-            } catch(e) {
+            // try{
+            //     this.ws.send({message: "start"})
+            // } catch(e) {
                 
-            }
-            
+            // }
+            console.log('open')
         }
 
         this.ws.onmessage = event => {
             const data = JSON.parse(event.data)
+            console.log(data)
+
+            this.setState({currencies: data.currencies.map(element => {
+                return {name: element.name, buy: element.buy, sell: element.sell}
+            })})
         }
     }
 
@@ -23,18 +42,18 @@ class Banner extends React.Component {
         return(
             <div className="banner">
                 <table>
-                <tbody>
-                    {['USD', 'EUR', 'RUB', 'KGS', 'GBP', 'CNY'].map((data, i) => {
+                <tbody className="table">
+                    {this.state.currencies.map((data) => {
                         return(
                             <tr>
                                 <td>
-                                    <Cell value={376} key={i}></Cell>
+                                    <Cell value={data.buy} key={data.name}></Cell>
                                 </td>
                                 <td className="currency-name">
-                                    {data}
+                                    {data.name}
                                 </td>
                                 <td>
-                                    <Cell value={378} key={data}></Cell>
+                                    <Cell value={data.sell} key={data.name+10}></Cell>
                                 </td>
                             </tr>
                         )
