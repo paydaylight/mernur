0016a70f49df35cf15baa4f6673911a2cd89fb72
paydaylight <maxat235@gmail.com>
2020-02-25 22:18:56 +0600

    removed console log

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -1,5 +1,5 @@
 import {initializeApp} from 'firebase';
-console.log(process.env.REACT_APP_FIREBASE_PROJECT_ID)
+
 const app = initializeApp({ 
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY
 });
